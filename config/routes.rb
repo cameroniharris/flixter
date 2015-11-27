9e29f65dda83c682db94add4d7bf395e5e563a49
Cameron Harris <cameroniharris1@gmail.com>
2015-11-27 21:06:50 +0000

    Preparing to put in image uploading

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ Flixter::Application.routes.draw do
   #   end
   root 'static_pages#index'
   resources :courses, :only => [:index,:show]
+  resources :lessons, :only => [:show]
   namespace :instructor do
     resources :sections, :only => [] do
       resources :lessons, :only => [:new, :create]
