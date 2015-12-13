17176f05302e960eeaf49a221cd117d570228afc
Cameron Harris <cameroniharris1@gmail.com>
2015-12-13 03:16:30 +0000

    added moveable lessons

--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,7 @@ Flixter::Application.routes.draw do
   resources :lessons, :only => [:show]
   resources :image, :only => :create 
   namespace :instructor do
+    resources :lessons, :only => [:update]
     resources :sections, :only => [] do
       resources :lessons, :only => [:new, :create]
     end
